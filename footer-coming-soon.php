31cba6c3b96969517eac0171ac1b0a308e5e2183
Erik <erikdmitchell@gmail.com>
2021-01-22 11:33:38 -0500

    added coming soon landing page

---
+++ footer-coming-soon.php
@@ -0,0 +1,16 @@
+        <footer class="coming-soon-footer">
+            <div class="footer-content">
+                <h3>Connect with Me</h3>
+                <ul class="social-media">
+                    <li id="social-media-facebook"><a href="https://www.facebook.com/erikdmitchell"><i class="fab fa-facebook"></i></a></li>
+                    <li id="social-media-twitter"><a href="https://twitter.com/erikdmitchell"><i class="fab fa-twitter"></i></a></li>
+                    <li id="social-media-instagram"><a href="https://instagram.com/erikdmitchell"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+
+                <div class="copyright"><?php echo get_bloginfo( 'name' ); ?> &copy; <?php echo date( 'Y' ); ?></div>
+            </div>
+        </footer>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ header-coming-soon.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="coming-soon-html">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class('coming-soon-body'); ?>>
+
+    <header id="masthead" class="site-header clearfix">
+    </header><!-- #masthead -->
+    
+    
+
+
+
+
+
+

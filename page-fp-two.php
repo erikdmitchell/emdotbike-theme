6c46de12a1c42f0512251529b5c5a7d6da1da8aa
Erik <erikdmitchell@gmail.com>
2021-08-17 14:02:49 -0400

    changing to a 2x2 grid

--- page-fp-two.php
+++ page-fp-two.php
@@ -5,33 +5,34 @@
 ?>
 <?php get_header(); ?>
   
-<?php $posts = get_posts( array( 'posts_per_page' => 3 ) ); ?>
+<?php $posts = get_posts( array( 'posts_per_page' => 4 ) ); ?>
 <?php $total_posts = count($posts); ?>
-  
-<h3>Custom</h3>
+<?php $col_counter = 1; ?>
 
 <?php if ( !empty($posts) ) : ?>
 
     <div class="flex-grid-fp total-posts-<?php echo $total_posts; ?>">
 
-        <div class="flex-col">
+
             <?php foreach ($posts as $key => $post) : ?>
     
-            <?php echo $key % 2 == 1 ? '</div><div class="flex-col">' : ''; ?> 
+
+                
                 <?php
                     $classes = '';
                     $image_size = 'home-grid';
                 ?>           
                 <div class="post-item post-item-<?php echo $key; ?> post-<?php echo $post->ID . $classes; ?>">
-    <!--                     <?php emdotbike_theme_post_thumbnail_custom( $post, $image_size ); ?> -->
-                        <h3><?php echo get_the_title( $post ); ?></h3>
-    <!--                     <div class="excerpt"><?php emdotbike_post_excerpt( $post, 25, '', ' <a href="' . get_permalink( $post->ID ) . '">read more...</a>' ); ?></div> -->
+                    <?php emdotbike_theme_post_thumbnail_custom( $post, $image_size ); ?>
+                    <h3><?php echo get_the_title( $post ); ?></h3>
+                    <div class="excerpt"><?php emdotbike_post_excerpt( $post, 25, '', ' <a href="' . get_permalink( $post->ID ) . '">read more...</a>' ); ?></div>
                 </div>
-               
             <?php endforeach; ?>
-        </div>
+
         
     </div>
 
 <?php endif; ?>
 
+<?php
+    get_footer();

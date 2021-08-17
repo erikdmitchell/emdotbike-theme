b5c6f2f4fc826bd0c4477d1194ecb8ca384e2d21
Erik <erikdmitchell@gmail.com>
2021-08-17 14:16:35 -0400

    two col with featured post in first

--- page-fp-two.php
+++ page-fp-two.php
@@ -6,29 +6,28 @@
 <?php get_header(); ?>
   
 <?php $posts = get_posts( array( 'posts_per_page' => 4 ) ); ?>
-<?php $total_posts = count($posts); ?>
-<?php $col_counter = 1; ?>
 
 <?php if ( !empty($posts) ) : ?>
 
-    <div class="flex-grid-fp total-posts-<?php echo $total_posts; ?>">
+    <div class="flex-grid front-page-grid">
 
-
+        <div class="flex-col">
             <?php foreach ($posts as $key => $post) : ?>
     
-
+                <?php emdotbike_insert_flex_col( $key ); ?>
                 
                 <?php
                     $classes = '';
                     $image_size = 'home-grid';
                 ?>           
-                <div class="post-item post-item-<?php echo $key; ?> post-<?php echo $post->ID . $classes; ?>">
+                <div class="flex-item post-<?php echo $post->ID . $classes; ?>">
                     <?php emdotbike_theme_post_thumbnail_custom( $post, $image_size ); ?>
                     <h3><?php echo get_the_title( $post ); ?></h3>
                     <div class="excerpt"><?php emdotbike_post_excerpt( $post, 25, '', ' <a href="' . get_permalink( $post->ID ) . '">read more...</a>' ); ?></div>
                 </div>
+               
             <?php endforeach; ?>
-
+        </div>
         
     </div>
 
@@ -36,3 +35,13 @@
 
 <?php
     get_footer();
+    
+function emdotbike_insert_flex_col($number = 0) {
+    $html = '';
+    
+    if (1 == $number % 3) {
+        $html .= '</div><div class="flex-col">';
+    }
+    
+    echo $html;   
+}

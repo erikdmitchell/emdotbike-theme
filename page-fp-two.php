121ad178b2a526006c5df63dbce0cbd7a654627b
Erik <erikdmitchell@gmail.com>
2021-08-17 12:17:45 -0400

    built out test page for new layout

---
+++ page-fp-two.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Front Page v2
+ **/
+?>
+<?php get_header(); ?>
+  
+<?php $posts = get_posts( array( 'posts_per_page' => 3 ) ); ?>
+<?php $total_posts = count($posts); ?>
+  
+<h3>Custom</h3>
+
+<?php if ( !empty($posts) ) : ?>
+
+    <div class="flex-grid-fp total-posts-<?php echo $total_posts; ?>">
+
+        <div class="flex-col">
+            <?php foreach ($posts as $key => $post) : ?>
+    
+            <?php echo $key % 2 == 1 ? '</div><div class="flex-col">' : ''; ?> 
+                <?php
+                    $classes = '';
+                    $image_size = 'home-grid';
+                ?>           
+                <div class="post-item post-item-<?php echo $key; ?> post-<?php echo $post->ID . $classes; ?>">
+    <!--                     <?php emdotbike_theme_post_thumbnail_custom( $post, $image_size ); ?> -->
+                        <h3><?php echo get_the_title( $post ); ?></h3>
+    <!--                     <div class="excerpt"><?php emdotbike_post_excerpt( $post, 25, '', ' <a href="' . get_permalink( $post->ID ) . '">read more...</a>' ); ?></div> -->
+                </div>
+               
+            <?php endforeach; ?>
+        </div>
+        
+    </div>
+
+<?php endif; ?>
+

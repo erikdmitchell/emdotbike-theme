aed859145ce50a5f95540d7ddefb90886a4a0cc0
Erik <erikdmitchell@gmail.com>
2021-09-08 10:01:20 -0400

    removed grid wrapper from content, using guten

--- page.php
+++ page.php
@@ -31,18 +31,18 @@ if ( have_posts() ) :
     while ( have_posts() ) :
         the_post();
         ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class( 'grid-wrapper' ); ?>>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <?php if ( is_search() ) : ?>
         <div class="entry-summary grid-row">
             <?php the_excerpt(); ?>
         </div><!-- .entry-summary -->
         <?php else : ?>
-        <div class="entry-content grid-row">
+        <div class="entry-content">
             <?php
-                the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'wpbootstrap' ) );
+                the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'emdotbike' ) );
                 wp_link_pages(
                     array(
-                        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'wpbootstrap' ) . '</span>',
+                        'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'emdotbike' ) . '</span>',
                         'after'       => '</div>',
                         'link_before' => '<span>',
                         'link_after'  => '</span>',

ef98f59eb42a72cd706f6400012b22eb3f3bf8c9
Erik <erikdmitchell@gmail.com>
2021-09-11 13:24:58 -0400

    added basic wrapper for content

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -39,42 +39,37 @@
         </div>
     </header>
         
-    <div class="grid-wrapper">
-        <div class="grid-row">
-            <div class="entry-content">
-                <?php
-                    the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'emdotbike' ) );
-                    wp_link_pages(
-                        array(
-                            'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'emdotbike' ) . '</span>',
-                            'after'       => '</div>',
-                            'link_before' => '<span>',
-                            'link_after'  => '</span>',
-                        )
-                    );
-                    ?>
-            </div><!-- .entry-content -->
-        </div>
-        
-        <div class="grid-row">
-            <div class="entry-meta">
-                <?php if ( has_tag() ) : ?>
-                    <div class="tags-list">
-                        <div class="tags-title">Tags</div>
-                        
-                        <?php the_tags( '<div class="tag-links">', ' ', '</div>' ); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if ( emdotbike_has_categories( 'Uncategorized' ) ) : ?>
-                    <div class="categories-list">
-                        <div class="categories-title">Categories</div>
-                    
-                        <div class="categories-link">
-                            <?php emdotbike_post_categories( ' ', 1 ); ?>
-                        </div>
-                    </div>
-                <?php endif; ?>
+    <div class="entry-content">
+        <?php
+            the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'emdotbike' ) );
+            wp_link_pages(
+                array(
+                    'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'emdotbike' ) . '</span>',
+                    'after'       => '</div>',
+                    'link_before' => '<span>',
+                    'link_after'  => '</span>',
+                )
+            );
+            ?>
+    </div><!-- .entry-content -->
+
+    <div class="entry-meta">
+        <?php if ( has_tag() ) : ?>
+            <div class="tags-list">
+                <div class="tags-title">Tags</div>
+                
+                <?php the_tags( '<div class="tag-links">', ' ', '</div>' ); ?>
             </div>
-        </div>
-    </div>            
+        <?php endif; ?>
+        <?php if ( emdotbike_has_categories( 'Uncategorized' ) ) : ?>
+            <div class="categories-list">
+                <div class="categories-title">Categories</div>
+            
+                <div class="categories-link">
+                    <?php emdotbike_post_categories( ' ', 1 ); ?>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+               
 </article><!-- #post-## -->

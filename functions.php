741cb1d881235f80804008b6959b8a6256b22797
Erik <erikdmitchell@gmail.com>
2021-09-28 08:55:43 -0400

    added inner-nav div to help with width on lg

--- functions.php
+++ functions.php
@@ -155,8 +155,8 @@ function emdotbike_theme_widgets_init() {
             'class' => 'emdb-nav',
             'before_widget' => '<div class="%2$s">',
             'after_widget' => '</div>',
-            'before_sidebar' => '<div id="%1$s" class="%2$s">',
-            'after_sidebar' => '</div>',
+            'before_sidebar' => '<div id="%1$s" class="%2$s"><div class="nav-inner">',
+            'after_sidebar' => '</div></div>',
             'before_title' => '<h3>',
             'after_title' => '</h3>',
         )

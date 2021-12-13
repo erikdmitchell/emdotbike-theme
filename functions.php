ff5dd38ad02968c8374a3cd8745101d59542a408
Erik <erikdmitchell@gmail.com>
2021-12-13 12:32:34 -0500

    disabled theme support for align wide

--- functions.php
+++ functions.php
@@ -69,7 +69,7 @@ function emdotbike_theme_setup() {
     add_theme_support( 'menus' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'title-tag' );
-    add_theme_support( 'align-wide' );
+    //add_theme_support( 'align-wide' );
     //add_theme_support( 'editor-styles' );
 
     /**

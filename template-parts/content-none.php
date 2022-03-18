dea2aebadc258bd395f8be6ccc177e32eb5add82
Erik Mitchell <erikdmitchell@gmail.com>
2022-03-17 22:10:03 -0400

    updated search and 404 content

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -11,10 +11,10 @@
 
 <div class="no-results not-found">
     <header class="page-header">
-        <h1 class="page-title"><?php _e( 'Nothing Found', 'emdotbike' ); ?></h1>
+        <h1 class="page-title"><?php _e( 'Keep on Riding', 'emdotbike' ); ?>...</h1>
     </header><!-- .entry-header -->
 
-    <div class="page-content">
+    <div class="entry-content">
 
         <?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
@@ -22,12 +22,14 @@
 
         <?php elseif ( is_search() ) : ?>
 
-            <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'emdotbike' ); ?></p>
+            <p><?php esc_html_e( 'Try searching again...it will make you faster in the end!', 'emdotbike' ); ?></p>
             <?php get_search_form(); ?>
 
         <?php else : ?>
 
-            <p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'emdotbike' ); ?></p>
+            <p><?php esc_html_e( 'Remember the days when your computer was fancy if it told you cadence?', 'emdotbike' ); ?></p> 
+            <p><?php esc_html_e( 'Whether you made a wrong turn, got dropped, or bonked - you need a little help to find where you are going.', 'emdotbike' ); ?></p> 
+            <p><?php esc_html_e( 'Like a wise old veteran racer, or that guy at the local gas station, try the search to get going again:', 'emdotbike' ); ?></p>
             <?php get_search_form(); ?>
 
         <?php endif; ?>

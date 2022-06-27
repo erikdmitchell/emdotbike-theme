fe17a5c42e4a85bc28bb24cb56e94d6691ed76d2
Erik Mitchell <erikdmitchell@gmail.com>
2022-06-27 10:24:00 -0400

    phpcs

--- templates/blocks/post-nav.php
+++ templates/blocks/post-nav.php
@@ -6,6 +6,8 @@
  * @param   string $content The block inner HTML (empty).
  * @param   bool $is_preview True during AJAX preview.
  * @param   (int|string) $post_id The post ID this block is saved to.
+ *
+ * @package EMdotBike
  */
 
 // Create id attribute allowing for custom "anchor" value.

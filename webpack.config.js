c479f186740524767cda937b891dd472c74539a3
Erik Mitchell <erikdmitchell@gmail.com>
2025-06-18 16:43:25 -0400

    added wp-scripts to npm, working on blocks

--- webpack.config.js
+++ webpack.config.js
@@ -1,208 +1,89 @@
-const path = require('path');
+const defaultConfig = require('@wordpress/scripts/config/webpack.config.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ZipPlugin = require('zip-webpack-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const path = require('path');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    mode: isProduction ? 'production' : 'development',
+  ...defaultConfig,
+  
+  entry: {
+    // Keep the default wp-scripts entries (handles src/index.js -> build/index.js)
+    ...defaultConfig.entry,
+    
+    // Add theme SASS entries
+    'theme-style': './sass/style.scss',
+    'theme-editor': './sass/editor.scss',
+  },
 
-    entry: {
-        // Main stylesheet
-        style: './sass/style.scss',
-        // Editor stylesheet
-        editor: './sass/editor.scss',
-        // JavaScript files
-        main: './src/js/main.js', // You may need to create this as an entry point
-    },
-
-    output: {
-        path: path.resolve(__dirname, 'assets'),
-        filename: 'js/[name]' + (isProduction ? '.min.js' : '.js'),
-        clean: false, // Don't clean the entire assets folder
-    },
-
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
-
-    module: {
-        rules: [
-            // SCSS/CSS processing
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
+  module: {
+    rules: [
+      // Keep all existing wp-scripts rules
+      ...defaultConfig.module.rules,
+      
+      // Add SASS rule specifically for theme files
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'sass'),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'autoprefixer',
                     {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                        },
+                      overrideBrowserslist: [
+                        'last 2 versions',
+                        '> 1%',
+                        'safari 5',
+                        'ie 8',
+                        'ie 9',
+                        'opera 12.1',
+                        'ios 6',
+                        'android 4',
+                      ],
+                      cascade: false,
                     },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        'autoprefixer',
-                                        {
-                                            overrideBrowserslist: [
-                                                'last 2 versions',
-                                                '> 1%',
-                                                'safari 5',
-                                                'ie 8',
-                                                'ie 9',
-                                                'opera 12.1',
-                                                'ios 6',
-                                                'android 4',
-                                            ],
-                                            cascade: false,
-                                        },
-                                    ],
-                                ],
-                            },
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                            sassOptions: {
-                                outputStyle: 'expanded',
-                            },
-                        },
-                    },
+                  ],
                 ],
+              },
             },
-
-            // JavaScript processing
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                outputStyle: 'expanded',
+              },
             },
-
-            // Font files
-            {
-                test: /\.(ttf|otf|eot|woff|woff2)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name][ext]',
-                },
-            },
-
-            // Image files
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext]',
-                },
-            },
+          },
         ],
-    },
-
-    plugins: [
-        // Remove empty JS files created from CSS-only entries
-        new RemoveEmptyScriptsPlugin(),
-
-        // Extract CSS into separate files
-        new MiniCssExtractPlugin({
-            filename: (pathData) => {
-                // Main style.scss always goes to root as style.css (no minification)
-                if (pathData.chunk.name === 'style') {
-                    return '../style.css';
-                }
-                // Other CSS files can be minified
-                return 'css/[name]' + (isProduction ? '.min.css' : '.css');
-            },
-        }),
-
-        // Copy static files for build
-        ...(isProduction ? [
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: '**/*',
-                        to: '../build',
-                        globOptions: {
-                            ignore: [
-                                '**/node_modules/**',
-                                '**/src/**',
-                                '**/sass/**',
-                                '**/vendor/**',
-                                '**/composer.json',
-                                '**/composer.lock',
-                                '**/gulpfile.js',
-                                '**/webpack.config.js',
-                                '**/package.json',
-                                '**/package-lock.json',
-                                '**/phpcs.ruleset.xml',
-                                '**/.stylelintrc',
-                                '**/svn/**',
-                                '**/*.scss',
-                                '**/assets/css/*.css', // Exclude non-minified CSS
-                                '**/assets/js/*.js',   // Exclude non-minified JS
-                                '!**/*.min.css',       // But include minified files
-                                '!**/*.min.js',
-                            ],
-                        },
-                    },
-                ],
-            }),
-
-            // Create zip file for distribution
-            new ZipPlugin({
-                path: '../',
-                filename: 'emdotbike.zip',
-                pathPrefix: 'build/',
-            }),
-        ] : []),
+      },
     ],
+  },
 
-    optimization: {
-        minimize: isProduction,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    format: {
-                        comments: false,
-                    },
-                },
-                extractComments: false,
-            }),
-            new CssMinimizerPlugin({
-                // Exclude style.css from minification
-                exclude: /style\.css$/,
-                minimizerOptions: {
-                    preset: [
-                        'default',
-                        {
-                            discardComments: { removeAll: true },
-                        },
-                    ],
-                },
-            }),
-        ],
-    },
-
-    resolve: {
-        extensions: ['.js', '.scss', '.css'],
-    },
-
-    stats: {
-        children: false,
-        entrypoints: false,
-        modules: false,
-    },
+  plugins: [
+    // Keep all wp-scripts plugins
+    ...defaultConfig.plugins,
+    
+    // Add plugins for theme SASS
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: (pathData) => {
+        if (pathData.chunk.name === 'theme-style') {
+          return '../style.css';
+        }
+        if (pathData.chunk.name === 'theme-editor') {
+          return '../assets/css/editor.css';
+        }
+        // Default behavior for blocks
+        return '[name].css';
+      },
+    }),
+  ],
 };

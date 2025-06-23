573f028f9421f6357ca2a5d5b86454ce324dba04
Erik Mitchell <erikdmitchell@gmail.com>
2025-06-23 11:50:25 -0400

    added ability to lint and fix scss

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities'), 'version' => 'c339b40380b5ceafd44e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities'), 'version' => 'baf2c2d4f7645b08b6f8');
